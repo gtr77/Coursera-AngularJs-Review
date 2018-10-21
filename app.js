780c8ec08c2b416078c35a4fa8122ea87a21dc30
Gustavo Romero <gtr77srv@gmail.com>
2018-10-20 20:02:52 -0500

    Set the initial configuration for branch Master.

--- app.js
+++ app.js
@@ -3,27 +3,10 @@
 
     angular
         .module('firstApp', [])
-        .controller('NameCalculatorController', NameCalculatorController)
+        .controller('mainController', initFunction)
     
     /** @ngInject */
-    function NameCalculatorController($scope){
-
-        $scope.name = "";
-        $scope.totalValue = 0;
-        
-        $scope.displayNumeric = function () {
-          var totalNameValue = calculateNumericForString($scope.name);
-          $scope.totalValue = totalNameValue;
-        };
-      
-        function calculateNumericForString(string) {
-          var totalStringValue = 0;
-          for (var i = 0; i < string.length; i++) {
-            totalStringValue += string.charCodeAt(i);
-          }
-      
-          return totalStringValue;
-        }
+    function initFunction($scope){
 
     }
 
