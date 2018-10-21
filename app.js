7a7f7cab6fb178b6f3670845d6fe03c162ba7ec4
Gustavo Romero <gtr77srv@gmail.com>
2018-10-21 00:54:04 -0500

    Assignment#1 - TextBox comma-separated items they usually eat for lunch

--- app.js
+++ app.js
@@ -8,6 +8,24 @@
     /** @ngInject */
     function initFunction($scope){
 
+        /**
+         * Initial State for Dishes
+         */
+        $scope.dishesValues = "";
+
+        $scope.message = "";
+
+        $scope.checkQuantityOfDishes = function() {
+            var dishesValues = $scope.dishesValues;
+            var dishes = dishesValues.split(",");
+            if ( !dishesValues ){
+                $scope.message = "Please enter data first";
+            } else if( dishes.length <= 3 ) {
+                $scope.message = "Enjoy!";
+            } else {
+                $scope.message = "Too much!";
+            }
+        }
     }
 
 }());
