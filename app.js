55c082f2cfe696fe2da9d0925c7f37a03c5cfa71
Gustavo Romero <gtr77srv@gmail.com>
2018-10-21 14:35:56 -0500

    Assignment#1 - Set Alert types into an object and add the alert validations

--- app.js
+++ app.js
@@ -15,15 +15,29 @@
 
         $scope.message = "";
 
+        $scope.alertTypes = {};
+        var alertType = $scope.alertTypes;
+
+        alertType = {
+            success : "success",
+            danger : "danger",
+            warning : "warning" 
+        }
+        
+        $scope.alertStyleType = "";
+
         $scope.checkQuantityOfDishes = function() {
             var dishesValues = $scope.dishesValues;
             var dishes = dishesValues.split(",");
             if ( !dishesValues ){
                 $scope.message = "Please enter data first";
+                $scope.alertStyleType = alertType.warning; 
             } else if( dishes.length <= 3 ) {
                 $scope.message = "Enjoy!";
+                $scope.alertStyleType = alertType.success; 
             } else {
                 $scope.message = "Too much!";
+                $scope.alertStyleType = alertType.danger; 
             }
         }
     }
