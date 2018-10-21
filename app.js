c0a6f6645c58b514cc8f3f44fccdf39f72213867
Gustavo Romero <gtr77srv@gmail.com>
2018-10-20 21:11:12 -0500

    Dependency Injection in Javascript         (and AngularJS)

--- app.js
+++ app.js
@@ -2,12 +2,31 @@
     'use strict';
 
     angular
-        .module('firstApp', [])
-        .controller('mainController', initFunction)
+        .module('DIApp', [])
+        .controller('DIController', DIController)
+
+    /* The $injector has a method called annotate which returns 
+    an array of the argument namesof a function value. */    
     
     /** @ngInject */
-    function initFunction($scope){
+    function DIController ( $scope,
+                            $filter,
+                            $injector) {
 
+        $scope.name = "Yaakov";
+
+        $scope.upper = function () {
+            var upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name);
+        };
+
+        console.log($injector.annotate(DIController));
     }
+
+    function AnnonateMe(name, job, blah) {
+        return "Blah!";
+    }
+
+    console.log(DIController.toString());
 
 }());
